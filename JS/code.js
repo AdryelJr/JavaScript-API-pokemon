53a484535cffc3173e5cc9176f4e03f682eb02c9
Adryel Santos <adryeljuniods@gmail.com>
2023-05-16 07:27:00 -0300

    update code

--- JS/code.js
+++ JS/code.js
@@ -1,39 +1,39 @@
-const pokemonImage = document.querySelector('.pokemon_image');
-const pesquisa = document.querySelector('.input-pesquisa');
-const form = document.querySelector('.form');
+// const pokemonImage = document.querySelector('.pokemon_image');
+// const pesquisa = document.querySelector('.input-pesquisa');
+// const form = document.querySelector('.form');
 
 
-let pesquisaPokemon = 1;
+// let pesquisaPokemon = 1;
 
 
-const fetchPokemon = async (pokemon) => {
-    const APIResnponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+// const fetchPokemon = async (pokemon) => {
+//     const APIResnponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
 
-    if(APIResnponse.status == 200){
-        const data = await APIResnponse.json();
-        return data;
-    }
-}
+//     if(APIResnponse.status == 200){
+//         const data = await APIResnponse.json();
+//         return data;
+//     }
+// }
 
-const renderPokemon = async (pokemon) => { 
+// const renderPokemon = async (pokemon) => { 
 
-    const data = await fetchPokemon(pokemon);
+//     const data = await fetchPokemon(pokemon);
     
-    if(data){
-        pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
-        pesquisa.value = '';
-        pesquisaPokemon = data.id;
-    }
-}
+//     if(data){
+//         pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+//         pesquisa.value = '';
+//         pesquisaPokemon = data.id;
+//     }
+// }
 
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    renderPokemon(pesquisa.value.toLowerCase());
-});
+// form.addEventListener('submit', (event) => {
+//     event.preventDefault();
+//     renderPokemon(pesquisa.value.toLowerCase());
+// });
 
 
 
-renderPokemon(pesquisaPokemon);
+// renderPokemon(pesquisaPokemon);
 
 
 
